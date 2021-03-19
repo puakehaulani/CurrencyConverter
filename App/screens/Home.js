0f12f99532439afdb3ed48307d5541255cb4d9d9
puakehaulani <puakehaulani@gmail.com>
2021-03-19 15:25:23 -0700

    measure keyboard height and account for it using effect on separate component

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, StatusBar, Image, Dimensions, Text, ScrollView, Keyboard } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, StatusBar, Image, Dimensions, Text, ScrollView } from 'react-native';
 import { format } from 'date-fns';
 
 import colors from '../constants/colors';
 import { ConversionInput } from '../components/ConversionInput';
 import { Button } from '../components/Button';
+import { KeyboardSpacer } from '../components/KeyboardSpacer'
 
 const screen = Dimensions.get('window');
 
@@ -52,22 +53,6 @@ export default () => {
 
     const [scrollEnabled, setScrollEnabled] = useState(false);
 
-    useEffect(() => {
-        const showListener = Keyboard.addListener('keyboardDidShow', () => {
-            setScrollEnabled(true);
-        });
-
-        const hideListener = Keyboard.addListener('keyboardDidHide', () => {
-            setScrollEnabled(false);
-        });
-
-        return () => {
-            showListener.remove();
-            hideListener.remove()
-        }
-    }, [])
-
-
     return (
         <View style={styles.container}>
             <ScrollView scrollEnabled={scrollEnabled}>
@@ -102,7 +87,8 @@ export default () => {
 
                     <Button text="Reverse Currencies" onPress={() => alert("todo!")} />
 
-                    <View style={{ height: screen.height }} />
+                    <KeyboardSpacer onToggle={(keyboardIsVisible) => setScrollEnabled(keyboardIsVisible)} />
+
                 </View>
             </ScrollView>
         </View>
