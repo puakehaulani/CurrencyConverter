573ee36eef69f0892617b0eeded5d222aaf62ec7
puakehaulani <puakehaulani@gmail.com>
2021-03-18 21:51:58 -0700

    set up home screen

---
+++ App/screens/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
+
+import colors from '../constants/colors';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.blue,
+        flex: 1
+    }
+})
+
+export default () => {
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+        </View>
+    )
+}
