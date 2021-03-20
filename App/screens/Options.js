bc0f890ca29ef8d153bec415ae7080dcf768c373
puakehaulani <puakehaulani@gmail.com>
2021-03-19 19:42:47 -0700

    fixed status bar color styles

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, Linking, Alert } from 'react-native';
+import { SafeAreaView, ScrollView, Linking, Alert, StatusBar } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 
 import colors from '../constants/colors';
@@ -14,6 +14,7 @@ const openUrl = (url) => {
 export default () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar barStyle="dark-content" backgroundColor={colors.white} />
             <ScrollView>
                 <RowItem
                     title="Themes"
