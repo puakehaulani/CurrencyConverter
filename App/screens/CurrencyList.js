855f003d9feed24d4fd721eff09d49a787bbae18
puakehaulani <puakehaulani@gmail.com>
2021-03-19 22:50:09 -0700

    context used on currency list screen to eliminate warning

--- App/screens/CurrencyList.js
+++ App/screens/CurrencyList.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StatusBar, FlatList, View, StyleSheet } from 'react-native';
 import { useSafeArea } from 'react-native-safe-area-context';
-import { Entypo } from "@expo/vector-icons";
+import { Entypo } from '@expo/vector-icons';
 
 import currencies from '../data/currencies.json';
 import { RowItem, RowSeparator } from '../components/RowItem';
 import colors from '../constants/colors';
+import { ConversionContext } from '../util/ConversionContext';
 
 const styles = StyleSheet.create({
     icon: {
         width: 30,
         height: 30,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
         backgroundColor: colors.blue,
-        borderRadius: 15,
-        alignItems: "center",
-        justifyContent: "center"
-    }
-})
+    },
+});
 
 export default ({ navigation, route = {} }) => {
-    const params = route.params || {};
     const insets = useSafeArea();
+    const {
+        baseCurrency,
+        quoteCurrency,
+        setBaseCurrency,
+        setQuoteCurrency,
+    } = useContext(ConversionContext);
+
+    const params = route.params || {};
+    const { isBaseCurrency } = params;
 
     return (
         <View
@@ -33,13 +42,22 @@ export default ({ navigation, route = {} }) => {
             <FlatList
                 data={currencies}
                 renderItem={({ item }) => {
-                    const selected = params.activeCurrency === item;
+                    let selected = false;
+
+                    if (isBaseCurrency && item === baseCurrency) {
+                        selected = true;
+                    } else if (!isBaseCurrency && item === quoteCurrency) {
+                        selected = true;
+                    }
+
                     return (
                         <RowItem
                             title={item}
                             onPress={() => {
-                                if (params.onChange) {
-                                    params.onChange(item);
+                                if (isBaseCurrency) {
+                                    setBaseCurrency(item);
+                                } else {
+                                    setQuoteCurrency(item);
                                 }
                                 navigation.pop();
                             }}
