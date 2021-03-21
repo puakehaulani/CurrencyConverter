def70599ec4e59ede5471624c53a131dd4a62438
puakehaulani <puakehaulani@gmail.com>
2021-03-20 19:36:26 -0700

    building out local api since actual one is not free anymore

--- App/index.js
+++ App/index.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import Navigation from './config/Navigation';
+import { api } from './util/api';
+
+api("/latest?base=USD")
+    .then(response => {
+        console.log("response", response)
+    })
+    .catch(error => {
+        console.log("error", error)
+    })
 
 export default () => <Navigation />;
